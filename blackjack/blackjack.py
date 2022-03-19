5ee8dbab7e31ec22f48e8b5a7cece06f3bbe99c4
Stas Goncharuk <goncharuk.sm@yandex.ru>
2022-03-19 08:48:58 +0300

    Added new project Blackjack. Defined constants of cards. TODO: define main func

---
+++ blackjack/blackjack.py
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+import random
+import sys
+
+# Set up the constants:
+HEARTS = chr(9829)
+DIAMONDS = chr(9830)
+SPADES = chr(9824)
+CLUBS = chr(9827)
+# (A list of chr codes is at https://inventwithpython.com/charactermap)
+BACKSIDE = 'backside'
+
+def main():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
