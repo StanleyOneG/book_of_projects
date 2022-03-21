9597589bae5c18261333ff2ecc9bd3d5e19249e7
Stas Goncharuk <goncharuk.sm@yandex.ru>
2022-03-21 17:37:29 +0300

    Defined get_move function

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -106,7 +106,24 @@ def display_hands(player_hand, dealer_hand, show_dealer_first):
     display_cards(player_hand)
 
 def get_move(player_hand, money):
-    pass
+    """ Asks the player for their move, and returns H for hit, S for stand,
+    and D for double down"""
+    while True: # Keep looping until the player enters correct move
+        # Determine what move the player can make:
+        moves = ['(H)it', '(S)tand']
+
+        # The player can double down in their first move
+        # the first move is when their have only two cards
+        if len(moves) == 2 and money > 0:
+            moves.append('(D)ouble down')
+
+        # Get the player's move:
+        move_choice = '\n'.join(moves) + '\n > '
+        move = input(move_choice).upper()
+        if move in ('H', 'S'):
+            return move # The player has entered correct move
+        if move == 'D' and '(D)ouble down' in moves:
+            return move # The player has entered correct move
 
 def main():
     print(''' Blackjack.
